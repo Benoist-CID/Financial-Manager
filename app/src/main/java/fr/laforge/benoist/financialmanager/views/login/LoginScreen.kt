6aa9c12a813f58a6c4e9d913f06de38faaf2a4c9
Darth Vader <benoist@DESKTOP-MC8AV35.localdomain>
2023-12-20 16:53:41 +0100

    Clean up

--- app/src/main/java/fr/laforge/benoist/financialmanager/views/login/LoginScreen.kt
+++ app/src/main/java/fr/laforge/benoist/financialmanager/views/login/LoginScreen.kt
@@ -1,6 +1,5 @@
 package fr.laforge.benoist.financialmanager.views.login
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -8,14 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -23,7 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -39,7 +34,6 @@ import androidx.navigation.compose.rememberNavController
 import fr.laforge.benoist.financialmanager.AppViewModelProvider
 import fr.laforge.benoist.financialmanager.FinancialManagerScreen
 import fr.laforge.benoist.financialmanager.R
-import timber.log.Timber
 
 @Composable
 fun LoginScreen(
@@ -96,7 +90,6 @@ fun LoginScreen(
             )
         }
     }
-
 
     if (displayBiometrics) {
         vm.displayBiometricAuthenticator(
