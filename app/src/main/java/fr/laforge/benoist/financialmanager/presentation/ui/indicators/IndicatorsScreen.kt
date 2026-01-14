f55d35c236e4813f7b9e68afde83d25d949a4ff1
Benoist Laforge <benoist.laforge@credenceid.com>
2026-01-14 17:58:44 +0100

    Adds back navigation arrow to Indicators screen

--- app/src/main/java/fr/laforge/benoist/financialmanager/presentation/ui/indicators/IndicatorsScreen.kt
+++ app/src/main/java/fr/laforge/benoist/financialmanager/presentation/ui/indicators/IndicatorsScreen.kt
@@ -10,9 +10,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -69,7 +73,15 @@ fun IndicatorsScreen(
                 },
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.background
-                )
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
+                    }
+                },
             )
         }
     ) { innerPadding ->
